dd8d20e2bb84270be0956a1b723a5d3d9176a0a8
Vidhi Saini <49809642+vidhisaini@users.noreply.github.com>
2020-10-06 21:03:32 +0530

    Update asgi.py

--- reddittoplinks/asgi.py
+++ reddittoplinks/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twittertoplinks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddittoplinks.settings')
 
 application = get_asgi_application()
