8f038dcdbd52cefd8e7e1e0825126d363288f559
Vidhi Saini <49809642+vidhisaini@users.noreply.github.com>
2020-10-06 21:03:58 +0530

    Update wsgi.py

--- reddittoplinks/wsgi.py
+++ reddittoplinks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twittertoplinks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reddittoplinks.settings')
 
 application = get_wsgi_application()
